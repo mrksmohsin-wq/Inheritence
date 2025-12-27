81a74729b21d2607b303000c31628b92caf15c87
Khadim Sajjad <242244668+mrksmohsin-wq@users.noreply.github.com>
2025-12-28 03:34:51 +0500

    Add files via upload

---
+++ Example_2/Example_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_2
+{
+    class Person
+    {
+        public string Name;
+        public int Age;
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Age: " + Age);
+        }
+    }
+    class Student : Person
+    {
+        public string RollNumber;
+
+        public void Study()
+        {
+            Console.WriteLine(Name + " is studying.");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.Name = "Mohsin"; // inherited
+            s.Age = 20; //inherited 
+            s.RollNumber = "CS101"; // owned
+            s.DisplayInfo(); // Inherited method
+            s.Study(); // owned method
+        }
+    }
+}
