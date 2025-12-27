19448c12c7e28697a75b57a3a9b8fe6e05ab4bd1
Khadim Sajjad <242244668+mrksmohsin-wq@users.noreply.github.com>
2025-12-28 03:24:25 +0500

    Add files via upload

---
+++ Example 1/Example 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_1
+{
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Animal is eating");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Dog is barking");
+        }
+    }
+    class Example_1
+    {
+        static void Main(string[] args)
+        {
+            Dog d = new Dog();
+            d.Eat();   // inherited method
+            d.Bark();  // own method
+
+        }
+    }
+}
